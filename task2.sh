5565c68476fa39e7e410fdbdca8dc687742f53b7
AkhilChawla <46402073+AkhilChawla@users.noreply.github.com>
2024-11-21 02:09:47 +0000

    Revert "task2 update"
    This reverts commit 2a37d2358f1e2f966894983087350bdb99e1047a.

--- task2.sh
+++ task2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -c "sample" file* |  grep -E ":[1-9]+[0-9]*$" | cut -d: -f1 | xargs grep -o "CSC510" | uniq -c | grep -E -v "^\s*[1-2] file" | cut -d: -f1 | gawk '{system("ls -l "$2 "| xargs echo "$1 ) }' | sort -k1,1nr -k6,6nr | gawk '{print $10}' |  cut -d/ -f2 | sed 's/file/filtered/g'
+grep -c "sample" file* | grep -E "[1-9][0-9]*$" | cut -d: -f1 | xargs grep -o "CSC510" | uniq -c | sort -r | grep -E -v "^\s*[1-2]" | cut -d: -f1 | grep -Eo "file_[0-9]+" | xargs ls -l | sort -k5,5nr | grep -Eo "file_[0-9]+" | sed 's/file/filtered/g'
